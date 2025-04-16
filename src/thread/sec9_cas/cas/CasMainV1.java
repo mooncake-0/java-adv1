e844f9936e25d3fe12bcf733db4187bf5da138ff
mooncake-0 <mooncake01@naver.com>
2025-04-16 23:44:12 +0900

    d

--- src/thread/sec9_cas/cas/CasMainV1.java
+++ src/thread/sec9_cas/cas/CasMainV1.java
@@ -9,10 +9,10 @@ public class CasMainV1 {
         AtomicInteger atomicInteger = new AtomicInteger(0);
         System.out.println("start val = " + atomicInteger.get());
 
-        boolean result1 = atomicInteger.compareAndSet(0, 100); // 비교하고 값이 맞다면 비교해라
+        boolean result1 = atomicInteger.compareAndSet(0, 100); // 비교하고 값이 맞다면 교체해라
         System.out.println("result1 = " + result1 + ", value = " + atomicInteger.get());
 
-        boolean result2 = atomicInteger.compareAndSet(0, 500); // 비교하고 값이 맞다면 비교해라
+        boolean result2 = atomicInteger.compareAndSet(0, 500); // 비교하고 값이 맞다면 교체해라
         System.out.println("result1 = " + result2 + ", value = " + atomicInteger.get());
 
     }
